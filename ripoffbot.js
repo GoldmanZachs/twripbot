13b6b0300e49ec776338049a0d6a01f15dd51afe
Zach Goldman <elevatorgoldman@aol.com>
2014-02-26 06:39:49 +0000

    i added twitter api

--- ripoffbot.js
+++ ripoffbot.js
@@ -4,4 +4,13 @@ var settings = require('DogeAPI.js');
 
 app.get('/tweets/:username', function(req, res) {
   var username = req.params.username;
+  
+  options = {
+    protocol: "http",
+    host: "api.twiiter.com",
+    pathname: "1/statuses/user_timeline.json",
+    query: { screen_name: username, count: 50 }
+  }  
+  
+  var twitterUrl = url.format(options);
 }  
