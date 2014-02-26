971fb550f09686717eaf97a55a50ce16dc192c9e
Zach Goldman <elevatorgoldman@aol.com>
2014-02-26 06:34:56 +0000

    i added var user

--- ripoffbot.js
+++ ripoffbot.js
@@ -1,6 +1,7 @@
 var request = require('request');
 var request = require('url');
+var settings = require('DogeAPI.js');
 
 app.get('/tweets/:username', function(req, res) {
-  
+  var username = req.params.username;
 }  
